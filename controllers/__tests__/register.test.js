97c2889017178c917ea01a5c776151f5670ad546
Zenab <zenab.bohra19@gmail.com>
2020-02-04 18:51:35 +1100

    Imports a library node-mocks-http to create exact replica of http request and response object and update register.test.js file

--- controllers/__tests__/register.test.js
+++ controllers/__tests__/register.test.js
@@ -1,98 +1,81 @@
 const handleRegister = require('../register');
+const httpMocks = require('node-mocks-http');
 const bcrypt = require('bcrypt');
 const knex = require('knex');
-const db = knex({client: 'sqlite3', connection: ':memory:'});
-const { mockResponse } = require('../../lib/testUtils');
+const db = knex({client: 'sqlite3', connection: ':memory:', useNullAsDefault: true});
+
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  res.status = jest.fn().mockReturnValue(res);
+});
 
 describe('handleRegister()', () => {
-
-  beforeAll(() =>
-    db.schema.createTable('login', function (table) {
+  beforeAll(() => {
+    return db.schema.createTable('login', (table) => {
       table.increments();
       table.string('email');
       table.string('hash');
-      table.timestamps();
       table.unique(['email']);
-    }).then(() =>
-      db.schema.createTable('users', function (table) {
-        table.increments();
-        table.string('name');
-        table.string('email');
-        table.dateTime('joined_date');
-        table.timestamps();
-        table.unique(['email']);
-      }))
-  );
+    })
+      .then(() => {
+        return db.schema.createTable('users', (table) => {
+          table.increments();
+          table.string('name');
+          table.string('email');
+          table.timestamp('joined_date');
+          table.timestamp('updated_at');
+          table.unique(['email']);
+        })
+      })
+  });
 
-
-  test('name is invalid', async () => {
-    const req = {
-      body: {
-        name: 123,
-      }
-    };
-    const res = mockResponse();
+  test('invalid name', async () => {
+    req.body.name = 123;
+    req.body.email = 'correct_email@gmail.com';
+    req.body.password = 'correct_password';
     await handleRegister(req, res, db, bcrypt);
     expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({err: 'invalid name'});
+    expect(res._getJSONData()).toEqual({err: 'invalid name'});
   });
 
-  test('email is invalid', async () => {
-    const req = {
-      body: {
-        name: 'johnny',
-        email: 123,
-      }
-    };
-    const res = mockResponse();
+  test('invalid email', async () => {
+    req.body.name = 'John';
+    req.body.email = 123;
+    req.body.password = 'correct_password';
     await handleRegister(req, res, db, bcrypt);
     expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({err: 'invalid email'});
+    expect(res._getJSONData()).toEqual({err: 'invalid email'});
   });
 
-  test('password is invalid', async () => {
-    const req = {
-      body: {
-        name: 'John',
-        email: 'john@gmail.com',
-        password: 12
-      }
-    };
-
-    const res = mockResponse();
-
+  test('invalid password', async () => {
+    req.body.name = 'John';
+    req.body.email = 'correct_email@gmail.com';
+    req.body.password = 123;
     await handleRegister(req, res, db, bcrypt);
     expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({err: 'password length should be between 3 and 30'});
+    expect(res._getJSONData()).toEqual({err: 'password length should be between 3 and 30'});
   });
 
-  test('successfully registers a user', async () => {
-    const req = {
-      body: {
-        name: 'John',
-        email: 'john@gmail.com',
-        password: 'john'
-      }
-    };
-    const res = mockResponse();
-
+  test('Registration is successful', async () => {
+    req.body.name = 'John';
+    req.body.email = 'john@gmail.com';
+    req.body.password = 'john';
     await handleRegister(req, res, db, bcrypt);
-    expect(res.json).toHaveBeenCalledWith({name: req.body.name, email: req.body.email});
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res._getJSONData()).toEqual({
+      name: req.body.name,
+      email: req.body.email
+    })
   });
 
-  test('email already exists in the database', async () => {
-    const req = {
-      body: {
-        name: 'John',
-        email: 'john@gmail.com',
-        password: 'john'
-      }
-    };
-    const res = mockResponse();
-
+  test('Registration is unsuccessful', async () => {
+    req.body.name = 'John';
+    req.body.email = 'john@gmail.com';
+    req.body.password = 'john';
     await handleRegister(req, res, db, bcrypt);
     expect(res.status).toHaveBeenCalledWith(500);
-    expect(res.json).toHaveBeenCalledWith({err: 'unable to register'});
-  });
-
+    expect(res._getJSONData()).toEqual({err: 'unable to register'});
+  })
 });
+
